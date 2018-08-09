4172c251574a13074eadc6fc2b6d5de52e8e2082
Mathieu Bergeron <mathieu@ciboulot.ca>
2018-08-09 14:25:53 -0400

    Reverse proxy 80 => 8080

---
+++ server/launch_proxied_prod.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+# vim: set fileencoding=utf-8 :
+#
+# Ciboulot: a simple Web App for text-based content and Angular.js delivery
+# Copyright (C) 2014 Mathieu Bergeron
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# load constants from config file
+# for prod profile
+import load_constants
+load_constants.load_profile('prod')
+
+# Launch prod server on 8080 ; apache will forward request from 80
+import server
+server.launch(8080)
